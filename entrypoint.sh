18b2901aa0c1894bb06a3748ba537aa988cd742d
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-29 17:24:20 -0500

    Update with some debugging information.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Hello World";
+DANGERFILE="$1";
+
+echo "DangerJS Github Action";
+echo "---";
+
+if [ -n "${DANGERFILE}" ]; 
+  then
+    echo "Dangerfile: ${DANGERFILE}";
+  else
+    echo "No dangerfile specified. Exiting with error...";
+    exit 1;
+fi
+
+echo "Contents of working directory:";
+echo "---";
+ls -la;
+echo "---";
