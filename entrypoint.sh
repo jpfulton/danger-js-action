08ace5fd687e9222aa7f3a452d4edefc15a5771e
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-29 17:33:53 -0500

    Another debugging run.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DANGERFILE="$1";
+DEBUG_MODE="$2";
+GITHUB_TOKEN="$3";
 
 echo "DangerJS Github Action";
 echo "---";
@@ -13,7 +15,16 @@ if [ -n "${DANGERFILE}" ];
     exit 1;
 fi
 
-echo "Contents of working directory:";
-echo "---";
-ls -la;
-echo "---";
+if [ -n "${DEBUG_MODE}" ] && [ "${DEBUG_MODE}" = "true" ]; 
+  then
+    echo "Contents of working directory:";
+    echo "---";
+    ls -la;
+    echo "---";
+    echo;
+
+    echo "Contents of the container action directory:";
+    echo "---";
+    ls -la /github/workspace;
+    echo "---";
+fi
