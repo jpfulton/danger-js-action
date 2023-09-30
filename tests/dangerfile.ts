a17953cd994e0ac1ede71295402e2422cc77b810
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-30 15:56:47 -0500

    Update test to work with license auditor plugin.

--- tests/dangerfile.ts
+++ tests/dangerfile.ts
@@ -1,5 +1,7 @@
 import { danger, warn } from "danger";
 
+import { licenseAuditor } from "@jpfulton/node-license-auditor-cli";
+
 export default async () => {
   if (!danger.github) {
     return;
@@ -9,10 +11,19 @@ export default async () => {
     warn("This run was not triggered by a PR and will not check for PR specific changes.");
     return;
   }
-
-  // No PR is too small to include a description of why you made a change
-  if (danger.github.pr.body.length < 10) {
-    warn("Please include a description of your PR changes.");
+  else {
+    // No PR is too small to include a description of why you made a change
+    if (danger.github.pr.body.length < 10) {
+      warn("Please include a description of your PR changes.");
+    }
   }
 
+  // Run the license auditor plugin
+  licenseAuditor({
+    failOnBlacklistedLicense: false,
+    projectPath: "/github/workspace", // path inside the docker container to the repo root
+    remoteConfigurationUrl: "https://raw.githubusercontent.com/jpfulton/jpfulton-license-audits/main/.license-checker.json",
+    showMarkdownSummary: true,
+    showMarkdownDetails: true,
+  });
 };
