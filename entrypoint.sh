ffe4ea48c87c1025b07fe5f51ac46a50841ade20
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-30 21:46:20 -0500

    Try passing token via input.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Collect arguments from action input variables to local variables
 DANGERFILE="$1";
 DEBUG_MODE="$2";
-#GITHUB_TOKEN="$3";
+GITHUB_TOKEN="$3";
 
 # Set local constants
 ACTION_WORKSPACE_DIR="/action/workspace";
